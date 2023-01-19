19569f72876f05e35f730dd31faa13cc8328d751
Milian Solberg <49562743+miliansolberg@users.noreply.github.com>
2023-01-19 13:29:31 +0100

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,62 @@
+<script>
+  import Navbar from './lib/Navbar.svelte'
+  import 'animate.css';
+
+
+function topScroll() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+</script>
+
+<main>
+  <Navbar />
+  <div class="h1container">
+  <h1 class=" animate__animated animate__bounceInLeft">HOLY GRAIL</h1>
+</div>
+  <div class="imgcontainer">
+  <img on:click={topScroll} src="https://www.freeiconspng.com/thumbs/trophy-png/trophy-png-23.png" class="trophy animate__animated animate__bounceInUp" alt="trophy">
+</div>
+</main>
+  <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter:wght@100;400&display=swap');
+
+
+.imgcontainer {
+  display: flex;
+  justify-content: center;
+  transform: translateY(-430px);
+}
+
+    .trophy {
+      transform: rotate(-20deg);
+      z-index: 2;
+      height: 30rem;
+    }
+
+    h1 {
+      transform: translateY(500%);
+      font-size: 32rem;
+      font-family: 'Bebas Neue', cursive;
+      text-align: center;
+      justify-content: center;
+    }
+
+    .h1container {
+    display:flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.trophy:hover {
+  scale: 1.1;
+  transition: 200ms;
+  cursor: pointer;
+}
+
+  </style>
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,40 @@
+<script>
+</script>
+
+<nav>
+<ul>
+    <li><a href="/">Home</a>
+        <li><a href="/">Shop</a>
+            <li><a href="/">Schedule</a>
+    </li>
+</ul>
+</nav>
+
+<style>
+
+    nav {
+
+    }
+
+    nav ul {
+    display: flex;
+    justify-content: space-between;
+    max-width: 14%;
+    margin: auto;
+}
+    nav ul li {
+    list-style-type: none;
+    font-weight: 600;
+    }
+    nav ul li a {
+    color: #fff;
+    text-decoration: none;
+    opacity: 0.8;
+    }
+
+    nav ul li a:active {
+        opacity: 1;
+        border: 2px solid greenyellow;
+        border-radius: 4px;
+    }
+</style>
